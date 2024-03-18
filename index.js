e81f43d874a38d2200a1dbd21b1c3aa398091ba1
wara <144021938+waraumbach@users.noreply.github.com>
2024-03-18 11:14:53 +0100

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+//import sum from "./sum.js";
+import chalk from "chalk";
+
+console.log(chalk.blue("hello world"));
+
+console.log(chalk.blue("marla"));
+console.log(chalk.blue("Sheikh"));
+console.log(chalk.black("Ben"));
+console.log(chalk.black("Alex"));
