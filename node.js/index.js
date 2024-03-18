e8619ba001711cce6afb12768733fa5bec05b881
wara <144021938+waraumbach@users.noreply.github.com>
2024-03-18 10:38:32 +0100

    Add files via upload

---
+++ node.js/index.js
@@ -0,0 +1,13 @@
+//const sum = require("./sum.js");
+
+import sum from "./sum.js";
+import chalk from "chalk";
+
+console.log(chalk.blue("hello world"));
+
+console.log(chalk.blue("marla"));
+console.log(chalk.blue("Sheikh"));
+console.log(chalk.black("Ben"));
+console.log(chalk.black("Alex"));
+
+sum(10, 20);
